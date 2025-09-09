3588ca016655839ff8a87c09f269b15ed0bdb669
danimuhidin <danimuhidin@gmail.com>
2025-09-09 20:31:09 +0700

    set api route

--- routes/motorinci.php
+++ routes/motorinci.php
@@ -1,15 +1,31 @@
 <?php
 
+use App\Http\Controllers\Motorinci\AvailableColorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Motorinci\BrandController;
 use App\Http\Controllers\Motorinci\CategoryController;
 use App\Http\Controllers\Motorinci\ColorController;
 use App\Http\Controllers\Motorinci\FeatureItemController;
+use App\Http\Controllers\Motorinci\MotorController;
+use App\Http\Controllers\Motorinci\MotorFeatureController;
+use App\Http\Controllers\Motorinci\MotorImageController;
+use App\Http\Controllers\Motorinci\MotorSpecificationController;
+use App\Http\Controllers\Motorinci\ReviewController;
+use App\Http\Controllers\Motorinci\SpecificationGroupController;
+use App\Http\Controllers\Motorinci\SpecificationItemController;
 
 Route::middleware('auth:sanctum')->prefix('motorinci')->group(function () {
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('brands', BrandController::class);
     Route::apiResource('colors', ColorController::class);
-    Route::apiResource('feature-items', FeatureItemController::class);
+    Route::apiResource('features', FeatureItemController::class);
+    Route::apiResource('specification-groups', SpecificationGroupController::class);
+    Route::apiResource('specification-items', SpecificationItemController::class);
+    Route::apiResource('available-colors', AvailableColorController::class);
+    Route::apiResource('motors', MotorController::class);
+    Route::apiResource('motor-features', MotorFeatureController::class);
+    Route::apiResource('motor-images', MotorImageController::class);
+    Route::apiResource('motor-specifications', MotorSpecificationController::class);
+    Route::apiResource('reviews', ReviewController::class);
 });
